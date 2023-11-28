aefde407312e31dcc06b0fbb5223a3902f1e491b
Rodrigo Conte <rodrigo.conte-terceiro@cea.com.br>
2023-11-28 11:03:36 -0400

    Feat: Included base to onboarding view - Created base of view with header, mid and footer - Created base of dragger on view

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -15,17 +15,101 @@ struct OnboardingView: View {
     // MARK: - Body
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button {
-                isOnboardingViewActive = false
-            } label: {
-                Text("Start")
-            }
-
-        }//: VStack
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                
+                Spacer()
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                }//:HEADER
+                
+                // MARK: - CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }//: ZSTACK
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }// :CENTER
+                Spacer()
+                
+                // MARK: - FOOTER
+                
+                ZStack {
+                    // 1. BACKGROUND
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. CALL TO ACTION
+                    
+                        Text("Get started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. CAPSULE
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // 4. Circle
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .foregroundColor(.white)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    } //: HSTACK
+                }//: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
+                
+            }//: VSTACK
+        }//: ZSTACK
     }
 }
 
