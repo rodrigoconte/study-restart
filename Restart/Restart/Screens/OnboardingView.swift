dfc1a81d69a8cc7981d9e92432121d7825c2f118
Rodrigo Conte <rodrigo.conte-terceiro@cea.com.br>
2023-12-05 19:59:13 -0400

    Feat: Included gesture on dragger - Add the gesture action on cutom dragger

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -12,6 +12,9 @@ struct OnboardingView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    
     // MARK: - Body
     
     var body: some View {
@@ -72,7 +75,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         
                         Spacer()
                     }
@@ -90,14 +93,28 @@ struct OnboardingView: View {
                         }
                         .frame(width: 80, height: 80, alignment: .center)
                         .foregroundColor(.white)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if gesture.translation.width > 0, buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                })
+                        )// : GESTURE
                         
                         Spacer()
                     } //: HSTACK
                 }//: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
                 
             }//: VSTACK
