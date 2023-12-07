6fa3282dabec9884b643c1e77d011d05f04c0ec2
Rodrigo Conte <rodrigo.conte-terceiro@cea.com.br>
2023-12-06 20:04:32 -0400

    Feat: Included more animations on onboarding view

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -15,6 +15,8 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
     
     // MARK: - Body
     
@@ -51,12 +53,46 @@ struct OnboardingView: View {
                 
                 ZStack {
                     CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1.2, y: 0)
+                        .rotationEffect(.degrees(Double(-imageOffset.width / 20)))
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                        }
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    withAnimation(Animation.easeOut(duration: 1)) {
+                                        imageOffset = .zero
+                                        indicatorOpacity = 1
+                                    }
+                                })
+                        )// :GESTURE
                 }// :CENTER
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity),
+                    alignment: .bottom
+                )
                 Spacer()
                 
                 // MARK: - FOOTER
